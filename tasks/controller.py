c0c44dfa0fe76b6de72ede332816bc9ac583e59a
Aditya Bidwai <46625413+adbidwai@users.noreply.github.com>
2020-10-25 17:20:36 +0530

    Update controller.py

--- tasks/controller.py
+++ tasks/controller.py
@@ -1,12 +1,23 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist 
-from nav_msgs.msg.msg import Odometry 
+from nav_msgs.msg.msg import Odometry
+from math import pi
+from tf import *
+global x,y,theta
 
 def callback(msg):
+  global x,y,theta
+  x = msg.odom.position.position.x
+  y = msg.odom.position.position.y
+  theta = fromQuaternionToEuler(msg.odom.pose.orientation)[2]
 
+twistMsg = Twist
 
 rospy.init_node("controller_node")
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
 sub = rospy.Subscriber('/odom', Odometry, callback)
-while not rospy.is_shutdown
+while (x < 1):
+  pub.publish(twistMsg(0.1,0,0,0,0,0))
+while(theta < pi/4):
+  pub.publish(twistMsg(0.1,0,0,0,0,0.5))
